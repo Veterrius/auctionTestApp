b97e3d50a89169a0c22abe9fce7633777aafc57f
veterr <avrukievich@gmail.com>
2021-11-15 21:05:09 +0300

    Added +-normal exception handling;

--- src/main/java/by/itstep/auction/controller/CustomExceptionHandler.java
+++ src/main/java/by/itstep/auction/controller/CustomExceptionHandler.java
@@ -7,6 +7,7 @@ import by.itstep.auction.service.exceptions.InvalidItemException;
 import by.itstep.auction.service.exceptions.NotEnoughMoneyException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.security.Principal;
@@ -14,35 +15,19 @@ import java.security.Principal;
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private final LotService lotService;
-    private final ItemService itemService;
-    private final UserService userService;
-
-    public CustomExceptionHandler(LotService lotService, ItemService itemService, UserService userService) {
-        this.lotService = lotService;
-        this.itemService = itemService;
-        this.userService = userService;
-    }
-
-//    @ExceptionHandler(InvalidItemException.class)
-//    public final ModelAndView handleInvalidItemException(InvalidItemException ex, Principal principal) {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("lots");
-//        modelAndView.addObject("message", ex.getMessage());
-//        modelAndView.addObject("lots", lotService.findAllLots());
-//        modelAndView.addObject("items", itemService.findItemsOfUser(userService.findUserByName(principal.getName())));
-//        return modelAndView;
-//    }
-
     @ExceptionHandler(InvalidItemException.class)
-    public String handleInvalidItemException(InvalidItemException ex, Principal principal) {
-        Long currentId = userService.findUserByName(principal.getName()).getId();
-        return "redirect:/lots/"+currentId;
+    public ModelAndView handleInvalidItemException(InvalidItemException ex, Principal principal) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("lotsError");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
     }
 
     @ExceptionHandler(NotEnoughMoneyException.class)
-    public String handleNotEnoughMoneyException(NotEnoughMoneyException ex, Principal principal) {
-        Long currentId = userService.findUserByName(principal.getName()).getId();
-        return "redirect:/lots/"+currentId;
+    public ModelAndView handleNotEnoughMoneyException(NotEnoughMoneyException ex, Principal principal) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("lotsError");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
     }
 }
